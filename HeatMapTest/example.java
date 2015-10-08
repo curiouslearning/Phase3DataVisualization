34cd040941e9b1f4653d04a704c4544116e90b50
Kate Harwood <kate@resnet133-17.medford.tufts.edu>
2015-10-08 18:17:04 -0400

    added data to heatmap

--- HeatMapTest/example.java
+++ HeatMapTest/example.java
@@ -6,15 +6,16 @@ import java.util.Random;
 
 
 
-import com.google.gson.Gson;
+/*import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
+import java.lang.reflect.Type;*/
+
 
 public class example {
 
    public static void main(String args[]) {
 
-      HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+      HashMap<String, Integer> hmap = new HashMap<String, Integer>();
 
       Random rand = new Random();
 
@@ -22,26 +23,35 @@ public class example {
       for(int i = 1420088400; i <= 1451538000; i+=86400){ // loop through days of year
 
         int randomNum = rand.nextInt((1000-0)+1); // random number of files sent by device (?)
-        String stringNum = Integer.toString(randomNum); // convert number to string
+        String timeStamp = Integer.toString(i); // convert timestamp to string
 
-        hmap.put(i, stringNum); // insert in Hashmap
+        hmap.put(timeStamp, randomNum); // insert in Hashmap
 
       }
 
-        Gson gson = new Gson ();
+
+      /*
+
+      JSONObject json = new JSONObject();
+      json.putALL(hmap);
+      System.out.printf("JSON: %s", json.toString(2)); */
+
+       /* Gson gson = new Gson ();
         String json = gson.toJson(hmap);
-        System.out.println("json = " + json);
+        System.out.println("json = " + json);*/
 
 
       
         // print out hashmap
-      /*Set set = hmap.entrySet();
+      Set set = hmap.entrySet();
       Iterator iterator = set.iterator();
+      System.out.print("{");
       while(iterator.hasNext()) {
          Map.Entry mentry = (Map.Entry)iterator.next();
-         System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-         System.out.println(mentry.getValue());
-      }*/
+         System.out.print("\""+ mentry.getKey() + "\": ");
+         System.out.println(mentry.getValue() + ",");
+      }
+      System.out.print("}");
 
      
 
